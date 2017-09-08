7997f84385c5728ee5db198d37280c32a1fac6cf
jimrain <jim@rainville.net>
2017-09-08 14:59:59 -0700

    First Commit

---
+++ Podfile
@@ -0,0 +1,10 @@
+source 'https://github.com/brightcove/BrightcoveSpecs.git'
+source 'https://github.com/CocoaPods/Specs.git'
+
+use_frameworks!
+
+platform :ios, '10.0' # (or whatever version you want)
+
+target 'VC_IMA_iOS_Swift' do
+  pod 'Brightcove-Player-IMA'
+end
---
+++ VC_IMA_iOS_Swift/VC-IMA-Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  VC-IMA-Bridging-Header.h
+//  VC_IMA_iOS_Swift
+//
+//  Created by James Rainville on 9/8/17.
+//  Copyright © 2017 James Rainville. All rights reserved.
+//
+
+#ifndef VC_IMA_Bridging_Header_h
+#define VC_IMA_Bridging_Header_h
+
+#import <BrightcoveIMA/BrightcoveIMA.h>
+#import <BrightcovePlayerSDK/BrightcovePlayerSDK.h>
+@import GoogleInteractiveMediaAds;
+
+#endif /* VC_IMA_Bridging_Header_h */
