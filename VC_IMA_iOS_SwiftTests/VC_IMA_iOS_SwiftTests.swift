f076b172d7f838a7beebe85e745e1302d3d1a368
jimrain <jim@rainville.net>
2017-09-08 14:14:32 -0700

    Initial Commit

---
+++ VC_IMA_iOS_SwiftTests/VC_IMA_iOS_SwiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  VC_IMA_iOS_SwiftTests.swift
+//  VC_IMA_iOS_SwiftTests
+//
+//  Created by James Rainville on 9/8/17.
+//  Copyright © 2017 James Rainville. All rights reserved.
+//
+
+import XCTest
+@testable import VC_IMA_iOS_Swift
+
+class VC_IMA_iOS_SwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
